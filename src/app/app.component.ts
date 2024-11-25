06ea1131307a31eed2f28748e176dcdbf738bf76
CCapparelli <kdcapparelli@gmail.com>
2024-11-25 16:24:42 -0300

    Componente de Imagem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImageTestComponent } from './image-test/image-test.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ImageTestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
