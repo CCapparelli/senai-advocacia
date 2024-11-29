0183b976b47c49c4b384a7bee59b214ac3f9d546
CCapparelli <kdcapparelli@gmail.com>
2024-11-28 21:33:38 -0300

    2024 11 28
    Rotas (conceito, PoC) etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { MasterComponent } from './master/master.component';
-import { SignupComponent } from './signup/signup.component';
-import { ReportsComponent } from './reports/reports.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-import { ClientComponent } from './client/client.component';
 
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
-    NavbarComponent, 
-    HomeComponent, 
-    ClientComponent,
-    LoginComponent, 
-    MasterComponent, 
-    SignupComponent, 
-    ReportsComponent,
-    FooterComponent, 
+    RouterOutlet, NavbarComponent, FooterComponent 
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
